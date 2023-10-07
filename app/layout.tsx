89853544baaaf7cb33ef7acb520cf585e2292014
Javier Muñoz Tous <javiermunoz.tous@biogen.com>
2023-10-07 11:08:21 +0200

    bug reproduction changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,27 @@
+import Script from "next/script";
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Script id="not-included" strategy="beforeInteractive">
+          {
+            `window.__NOT_INCLUDED_ENV = ${JSON.stringify({
+              foo: 'bar'
+            })}`
+          }
+        </Script>
+        <Script id="included">
+          {
+            `window.__INCLUDED_ENV = ${JSON.stringify({
+              foo: 'bar'
+            })}`
+          }
+        </Script>
+      </body>
     </html>
   )
 }
